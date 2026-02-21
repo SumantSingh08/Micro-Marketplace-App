d3ad0e602d530e57d2b6cf5e1c88c0bb4228fd14
SumantSingh08 <sumant082003@gmail.com>
2026-02-21 14:50:47 +0530

    first-commit

--- frontend/src/service/Api.js
+++ frontend/src/service/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://micro-marketplace-app-backend-pfxe.onrender.com",
 });
 
 instance.interceptors.request.use((config) => {
